77afde643e027c2d4d1b30e0306b5fd3bfff2047
Ubuntu <khalid@khalid-jenkins.t1pladknh2auzdbkljjtjjfdwg.fx.internal.cloudapp.net>
2022-01-03 22:41:42 +0000

    deployment stage file change

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,4 +2,6 @@
 
 echo "deploy stage"
 scp docker-compose.yml jenkins@swarm-manager:/home/jenkins/docker-compose.yml
-ssh jenkins@swarm-manager docker stack deploy --compose-file docker-compose.yml bootcamp-project
+ssh jenkins@swarm-manager \
+    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    docker stack deploy --compose-file docker-compose.yml bootcamp-project
