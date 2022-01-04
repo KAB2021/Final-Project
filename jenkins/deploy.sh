0d7d2efab5ede3448212893332d347ef561c3f9f
Ubuntu <khalid@khalid-jenkins.t1pladknh2auzdbkljjtjjfdwg.fx.internal.cloudapp.net>
2022-01-04 00:00:14 +0000

    fixing issues with docker

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,5 +4,6 @@ echo "deploy stage"
 scp docker-compose.yml jenkins@swarm-manager:/home/jenkins/docker-compose.yml
 ssh jenkins@swarm-manager \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
-    export MYSQL_ROOT_PASSWORD=khalid CREATE_SCHEMA=false \
-    docker stack deploy --compose-file docker-compose.yml bootcamp-project
+    docker stack deploy --compose-file docker-compose.yml bootcamp-project \
+    export MYSQL_ROOT_PASSWORD=khalid CREATE_SCHEMA=false
+    
