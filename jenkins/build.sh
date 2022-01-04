5bc46bacba07c8fa0833d6262f461d2a6047571e
Ubuntu <khalid@khalid-jenkins.t1pladknh2auzdbkljjtjjfdwg.fx.internal.cloudapp.net>
2022-01-04 01:34:53 +0000

    fixing issues with docker

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "build stage"
-# export MYSQL_ROOT_PASSWORD=khalid CREATE_SCHEMA=false
+export MYSQL_ROOT_PASSWORD=khalid CREATE_SCHEMA=false
 docker-compose build
--- jenkins/push.sh
+++ jenkins/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "push stage"
-MYSQL_ROOT_PASSWORD=khalid CREATE_SCHEMA=false
+
 docker-compose push
