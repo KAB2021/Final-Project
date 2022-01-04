a11b623b5da722909c1f3dbd21bc12f9706e425a
Ubuntu <khalid@khalid-jenkins.t1pladknh2auzdbkljjtjjfdwg.fx.internal.cloudapp.net>
2022-01-04 03:10:56 +0000

    fixing issues with docker

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,7 +2,7 @@
 
 echo "deploy stage"
 scp docker-compose.yml jenkins@swarm-manager:/home/jenkins/docker-compose.yml
-checkout scm docker.image(mysql:5.7).withRun(-e MYSQL_ROOT_PASSWORD= khalid -p 3306:3306)
+scm docker.image('mysql:5.7').withRun('-e "MYSQL_ROOT_PASSWORD= khalid" -p 3306:3306')
 ssh jenkins@swarm-manager \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
     docker stack deploy --compose-file docker-compose.yml bootcamp-project \
